068a0a1ece5b171473cc53691cbe785bd25fd0a9
Hai Do Minh <minhhai3b@gmail.com>
2015-03-26 19:53:16 +0900

    Add Marionette Lib

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -2,6 +2,18 @@ var app = app || {};
 
 //開始
 (function(app) {
-	var todoRouter = new app.TodoRouter();
-	Backbone.history.start();
+	// var todoRouter = new app.TodoRouter();
+	// Backbone.history.start();
+
+	app.Application = Backbone.Marionette.Application.extend({
+		
+		initialize : function(){
+			new app.TodoRouter();
+        },
+
+        onStart : function(){
+        	Backbone.history.start();
+        },
+    });
+
 })(app);
